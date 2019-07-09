6c37518dc39fa0cd7d687f75030dc2f4c1768a20
Backyardcrypto <alex.scagz@backyardcrypto.com>
2019-07-09 15:21:16 -0400

    add google oauth keys

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-
-app.get('/', (req, res) => {
-	res.send({ bye: 'bruh' });
-});
+passport.use(new GoogleStrategy());
 
 // dyanimcally figure out which port to listen to. Heroku will pass in environment variable for PORT for proudction env.
 const PORT = process.env.PORT || 5000;
