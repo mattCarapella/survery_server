f92d9b7999f485f6dfb44fec8a4ed408bf3c186d
Backyardcrypto <alex.scagz@backyardcrypto.com>
2019-07-11 09:49:18 -0400

    add user serialization/deserialization

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
+
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 const app = express();
+
+// Tell Express it needs to make use of cookies
+app.use(
+	cookieSession({
+		// configuration object 
+		maxAge: 30 * 24 * 60 * 60 * 1000,  // cookie expires after 30 days (described in ms)
+		keys: [keys.cookieKey] 	// used to sign/encrypt cookie. Defined in keys.js
+	})
+)
 
 require('./routes/authRoutes')(app);
 // same as:
@@ -15,3 +27,4 @@ require('./routes/authRoutes')(app);
 // dyanimcally figure out which port to listen to. Heroku will pass in environment variable for PORT for proudction env.
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
